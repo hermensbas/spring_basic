f2fa16a7c46708f55571b6d250806757c0d6cef2
bash <31279994+hermensbas@users.noreply.github.com>
2024-09-05 23:06:12 +0200

    Update WebClientConfig.java

--- src/main/java/nl/overheid/koop/basic/application/config/WebClientConfig.java
+++ src/main/java/nl/overheid/koop/basic/application/config/WebClientConfig.java
@@ -2,4 +2,9 @@ package nl.overheid.koop.basic.application.config;
 
 // configuration for creating new webclient and propagate the correlation-id place holder
 public class WebClientConfig {
+
+ // Webclient, Java HttpClient, okhttp, 
+ // apache httpClient, blocking, none blocking?
+ // reactive, futures, traditional threads
+ // which, why should we choose?
 }
